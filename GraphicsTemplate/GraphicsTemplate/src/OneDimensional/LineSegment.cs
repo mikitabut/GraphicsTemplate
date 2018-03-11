8f2679691327d058383f1ef222c362324e499462
thatglance <11sashamak@gmail.com>
2018-03-12 02:43:59 +0300

    feat(Ray&LineSegment): add Ray&LineSegment implementation

--- GraphicsTemplate/GraphicsTemplate/src/OneDimensional/LineSegment.cs
+++ GraphicsTemplate/GraphicsTemplate/src/OneDimensional/LineSegment.cs
@@ -1,9 +1,16 @@
+using System.Drawing;
+using System.Collections.Generic;
+using System;
 public class LineSegment : Ray {
 
 	public LineSegment(){
 
 	}
 
-	public void draw() { }
+	public override void draw(Graphics graphics) {
+        Pen pen = new Pen(this.getBorderColor());
+        graphics.DrawLine(pen, this.getCenter(), this.GetSecondPoint());
+        pen.Dispose();
+    }
 
 }//end LineSegment
--- GraphicsTemplate/GraphicsTemplate/src/OneDimensional/Ray.cs
+++ GraphicsTemplate/GraphicsTemplate/src/OneDimensional/Ray.cs
@@ -1,9 +1,32 @@
+using System.Drawing;
+using System;
 public class Ray : Line {
 
 	public Ray(){
 
 	}
 
-    public void draw() { }
+    public override void draw(Graphics graphics)
+    {
+        Pen pen = new Pen(this.getBorderColor());
+        Tuple<Point, Point> tuple = this.ResizeRay(this.getCenter(), this.GetSecondPoint(), 100000);
+        graphics.DrawLine(pen,
+            tuple.Item1,
+            tuple.Item2);
+        pen.Dispose();
+    }
+
+    private Tuple<Point, Point> ResizeRay(PointF start, PointF end, int length)
+    {
+        //Считаем вектор
+        var v = new PointF(end.X - start.X, end.Y - start.Y);
+        //Длина вектора
+        var l = (float)Math.Sqrt(v.X * v.X + v.Y * v.Y);
+        //Нормирование
+        v = new PointF(v.X / l, v.Y / l);
+        //Новые координаты отрезка
+        var newEnd = new Point((int)(end.X + v.X * length), (int)(end.Y + v.Y * length));
+        return new Tuple<Point, Point>(new Point((int)start.X, (int)start.Y), newEnd);
+    }
 
 }//end Ray
